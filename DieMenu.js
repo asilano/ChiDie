3e2a107587ff27072c65d8c41cb851b2c91a8211
Chris Howlett <chris.howlett@freeagent.com>
2024-01-07 15:40:31 +0000

    Wire up tip link to KoFi

--- DieMenu.js
+++ DieMenu.js
@@ -1,13 +1,25 @@
-import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { Component, useCallback } from "react";
+import { View, Text, StyleSheet, Linking, Alert } from "react-native";
 import Colours from "./Colours";
 import ScreenCommon from "./ScreenCommon";
 import Styles from "./Styles";
 
 const DieMenu = () => {
+  const tipLink = 'https://ko-fi.com/chowlett';
+  const visitTipLink = useCallback(async () => {
+    const supported = Linking.canOpenURL(tipLink);
+    if (supported) {
+      await Linking.openURL(tipLink);
+    } else {
+      Alert.alert("Sorry, we couldn't visit the tip page.");
+    }
+  }, [visitTipLink]);
+
   return (
     <ScreenCommon title='Choose die size'>
-      <Text style={Styles.body_text}>Like it? <Text style={Styles.link_text}>Leave me a tip! Not that this works.</Text></Text>
+      <Text style={Styles.body_text}>Like it?&nbsp;
+        <Text style={Styles.link_text} onPress={visitTipLink}>Leave me a tip!</Text>
+      </Text>
     </ScreenCommon>
   )
 }
